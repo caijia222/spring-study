e01da5d4913efe81d17b65e65a0f96400c23a628
caijia222 <caijia222@aliyun.com>
2021-08-20 20:07:26 +0800

    commit

--- src/main/java/com/caijia/web/UserController.java
+++ src/main/java/com/caijia/web/UserController.java
@@ -2,6 +2,8 @@ package com.caijia.web;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -32,16 +34,28 @@ public class UserController {
 	public ModelAndView signin() {
 		return new ModelAndView("signin.html");
 	}
-
+	
+	@GetMapping("/signout")
+	public ModelAndView signout(HttpSession session) {
+		session.removeAttribute("user");
+		return new ModelAndView("index.html");
+	}
+	
 	@PostMapping("/signin")
-	public ModelAndView signin(@RequestParam String email, @RequestParam String password) {
+	public ModelAndView signin(@RequestParam String email, @RequestParam String password, HttpSession session) {
 		User user = userService.signin(email, password);
+		session.setAttribute("user", user);
 		return new ModelAndView("index.html", "user", user);
 	}
 
 	@GetMapping("/user/profile")
-	public ModelAndView profile() {
-		return new ModelAndView("profile.html");
+	public ModelAndView profile(HttpSession session) {
+		User user = (User)session.getAttribute("user");
+		if(user != null) {
+			return new ModelAndView("profile.html","user",user);
+		}else {
+			return new ModelAndView("index.html");
+		}
 	}
 
 	@PostMapping(value = "/rest", consumes = "application/json;charset=utf-8", produces = "application/json;charset=utf-8")
